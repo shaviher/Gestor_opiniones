5b603ce2d3db5676927031ec25027205919d9732
shaviher <javiherdeza@gmail.com>
2025-02-20 23:58:59 -0600

    feat(auth): agrega función de login de usuario

---
+++ src/auth/auth.controller.js
@@ -0,0 +1,71 @@
+import { hash, verify } from "argon2"
+import User from "../user/user.model.js"
+import {  generateJWT } from "../helpers/generate-jwt.js"
+
+
+export const register = async (req, res) => {
+    try{
+        const data = req.body
+        let profilePicture = req.file ? req.file.filename : null
+        const encryptedPassword = await hash(data.password)
+        data.password = encryptedPassword
+        data.profilePicture = profilePicture
+
+        const user = await User.create(data)
+
+        return res.status(201).json({
+            successe: "User has been created",
+            name: user.name,
+            email: user.email
+        })
+    
+    }catch(err){
+        return res.status(500).json({
+            successe: false,
+            message: "login failed, server error",
+            errror: err.message
+            
+        })
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, username, password } = req.body
+    try{
+        const user = await User.findOne({
+            $or:[{email: email}, {username: username}]
+        })
+
+        if(!user){
+            return res.status(400).json({
+                message: "Invalid credentials",
+                error: "The username or email entered does not exist"
+            })
+        }
+
+        const validPassword = await verify(user.password, password)
+
+        if(!validPassword){
+            return res.status(400).json({
+                message: "Invalid credentials",
+                error: "Incorrect password"
+            })
+        }
+
+        const token = await generateJWT(user.id)
+
+        return res.status(200).json({
+            message: "Login successful",
+            userDetails: {
+                token: token,
+                profilePicture: user.profilePicture
+            }
+        })
+
+    }catch(err){
+        return res.status(500).json({
+            message: "login failed, server error",
+            error: err.message
+        })
+    }
+}
