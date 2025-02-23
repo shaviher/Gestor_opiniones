208a21fec67050d04585437621305ac258e82068
shaviher <javiherdeza@gmail.com>
2025-02-22 18:58:00 -0600

    feat: mejorar función de agregar comentario en publicaciones

--- src/comments/comments.controller.js
+++ src/comments/comments.controller.js
@@ -1,3 +1,4 @@
+import { populate } from "dotenv"
 import Comments from "./comments.model.js"
 
 export const addComments = async (req, res) => {
@@ -6,18 +7,43 @@ export const addComments = async (req, res) => {
         const { publication_id } = req.params
         const { text } = req.body
 
+        const publication = await Publications.findById(publication_id)
+        if(!publication) {
+            return res.status(404).json({
+                success: false,
+                message: "The publication is not exist"
+            })
+        }
+
         const newComments = new Comments({
             text,
             user: user._id,
             publication: publication_id
         })
 
-        await newComments.save()
+        const saveComments = await newComments.save()
+
+        publication.comments.push(saveComments._id)
+
+        await publication.save()
+
+        const publicationWithComments = await Publications.findById(publication_id).populate({
+            path: "comments",
+            select: "text -_id",
+            populate: {
+                path: "user",
+                select: "username -_id"
+            }
+        })
 
         return res.status(201).json({
             success: true,
             message: "Comment created successfully",
-            comment: newComments
+            publication: {
+                title: publicationWithComments.title,
+                text: publicationWithComments.text,
+                comments: publicationWithComments.comments
+            }
         })
 
     }catch(err){
